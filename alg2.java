1e59e0ad06842436083d3ad19509f7b9cb8cf53a
DiegoArteagaMendoza <daarteagamendoza@gmail.com>
2025-05-09 22:31:39 -0400

    algoritmo 2 arreglo

--- alg2.java
+++ alg2.java
@@ -7,38 +7,24 @@ public class alg2 {
         Arrays.sort(copia);
 
         if ("descendente".equalsIgnoreCase(sentido)) {
-            invertir(copia);
-        } else if (!"ascendente".equalsIgnoreCase(sentido)) {
-            throw new IllegalArgumentException("Sentido inválido: use 'ascendente' o 'descendente'.");
+            for (int i = 0, j = copia.length - 1;  i < j; i++, j--) {
+                int temp = copia[i];
+                copia[i] = copia[j];
+                copia[j] = temp;
+            }
         }
-
         return copia;
     }
-
-    // Invertir el arreglo
-    private static void invertir(int[] A) {
-        for (int i = 0, j = A.length - 1; i < j; i++, j--) {
-            int temp = A[i];
-            A[i] = A[j];
-            A[j] = temp;
-        }
-    }
-
     // Calcular la moda
     public static int moda(int[] A, String sentido) {
-        if (A.length == 0) {
-            throw new IllegalArgumentException("El arreglo no puede estar vacío.");
-        }
-
         int[] ordenado = ordenar(A, sentido);
-
         int moda = ordenado[0], frecuenciaMax = 1, frecuenciaActual = 1;
 
         for (int i = 1; i < ordenado.length; i++) {
             if (ordenado[i] == ordenado[i - 1]) {
                 frecuenciaActual++;
             } else {
-                if (frecuenciaActual > frecuenciaMax) {
+                if (frecuenciaActual > frecuenciaMax || (frecuenciaActual == frecuenciaMax && ordenado[i-1] < moda)) {
                     frecuenciaMax = frecuenciaActual;
                     moda = ordenado[i - 1];
                 }
@@ -46,8 +32,7 @@ public class alg2 {
             }
         }
 
-        if (frecuenciaActual > frecuenciaMax) {
-            frecuenciaMax = frecuenciaActual;
+        if (frecuenciaActual > frecuenciaMax || (frecuenciaActual == frecuenciaMax && ordenado[ordenado.length - 1] < moda)) {
             moda = ordenado[ordenado.length - 1];
         }
 
